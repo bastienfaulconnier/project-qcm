f1320b192776c5001c12cf4a29de6c694c09ce9b
bastienfaulconnier <bastien.faulconnier@gmail.com>
2017-08-01 12:56:50 +0200

    Ajouts exemple de routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::get('/', 'FrontController@index')->name('home'); // route('home') // construit l'url  
+
+Route::get('/posts/{id}','FrontController@showPost');
+Route::get('/contact', 'FrontController@showContact');
+Route::get('/mentionslegales', 'FrontController@showLegalNotice');
+Route::get('/lycee', 'FrontController@showmegaLycee');
+
+Route::any('login', 'Admin\LoginController@login')->name('login');
+Route::get('logout', 'Admin\LoginController@logout')->name('logout');
+
+/* Back Profs */
+/* Route::group(['middleware' => 'auth'], function(){
+	Route::get('dashboard', 'Admin\DashboardController@index');
+	Route::resource('admin/robot', 'Admin\RobotController');
+}); */
+
+/* Back Eleves */
+/* Route::group(['middleware' => 'auth'], function(){
+	Route::get('dashboard', 'Admin\DashboardController@index');
+	Route::resource('admin/qcmlist', 'Admin\RobotController');
+}); */
