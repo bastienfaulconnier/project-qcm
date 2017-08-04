ba977363df218dc0b22c453998b724aba879be85
Arkhad <emmanueldlh@gmail.com>
2017-08-04 12:17:27 +0200

    Modification nav.blade.php .

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,8 +2,5 @@
 	<a href="{{ url('/') }}">Accueil</a>
 	<a href="{{ url('posts') }}">Posts</a>
 	<a href="{{ url('lycee') }}">Lycée</a>
-	<a href="{{ url('contact') }}">Contact</a>
-	<a href="{{ url('mentionslegales') }}">Mentions légales</a>
-
 	<a href="{{ url('login') }}">Login</a>
 </nav>
