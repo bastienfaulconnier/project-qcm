222365a04422cc29378a524207aee72eb6db5c49
Arkhad <emmanueldlh@gmail.com>
2017-08-04 11:40:23 +0200

    Création méthode Logout, mise en place de la navigation, gestion des routes.

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,9 @@
+<nav>
+	<a href="{{ url('/') }}">Accueil</a>
+	<a href="{{ url('posts') }}">Posts</a>
+	<a href="#">Lycée</a>
+	<a href="#">Contact</a>
+	<a href="#">Mentions légales</a>
+
+	<a href="{{ url('login') }}">Login</a>
+</nav>
