ed2e113d5621a5b2e75798c47b060ea3cb13aa2e
Arkhad <emmanueldlh@gmail.com>
2017-08-09 14:35:27 +0200

    Modification route, view et back, plus création du middleware CheckRole.

--- resources/views/partials/front/nav.blade.php
+++ resources/views/partials/front/nav.blade.php
@@ -2,5 +2,11 @@
 	<a href="{{ url('/') }}">Accueil</a>
 	<a href="{{ url('posts') }}">Posts</a>
 	<a href="{{ url('lycee') }}">Lycée</a>
-	<a href="{{ url('login') }}">Login</a>
+	|
+	@if(auth()->check())
+        <a href="{{route('logout')}}">Se déconnecter</a>
+        <a href="{{route('teacher/home')}}">dashboard</a>
+    @else
+		<a href="{{route('login')}}">Login</a>
+    @endif
 </nav>
